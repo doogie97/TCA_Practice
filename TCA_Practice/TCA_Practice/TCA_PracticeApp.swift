3f729f72d0ebbc6270402c573562568d3346bb46
Doogie <chris_97@naver.com>
2024-05-09 18:20:30 +0900

    chore: 프로젝트 생성 및 라이브러리 설치

---
+++ TCA_Practice/TCA_Practice/TCA_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_PracticeApp.swift
+//  TCA_Practice
+//
+//  Created by Doogie on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
